696b984388798f40e7575d9c607c392ee2f426c5
I Wayan Natha Wiguna <nathawiguna1@gmail.com>
2022-05-06 12:02:23 +0800

    menghapus fungsi tidak berguna di DaoClass

--- app/src/main/java/com/example/Yassalam_Notebook/LihatCatatan.java
+++ app/src/main/java/com/example/Yassalam_Notebook/LihatCatatan.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class LihatCatatatan extends AppCompatActivity {
+public class LihatCatatan extends AppCompatActivity {
     Catatan catatanDilihat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
